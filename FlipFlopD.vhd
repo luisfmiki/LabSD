20f95d69992618e74fc0f2f92aaafafd648f1ab4
gccostaa <94900894+gccostaa@users.noreply.github.com>
2022-12-06 17:21:10 -0300

    Datapath/FlipFlopD

---
+++ FlipFlopD.vhd
@@ -0,0 +1,26 @@
+LIBRARY IEEE;
+use ieee.std_logic_1164.all;
+
+entity FlipFlopD is
+	port( clock: in std_logic;
+		  D: in std_logic;
+	      Q: out std_logic;
+			reset: in std_logic
+	    );
+end FlipFlopD;
+
+architecture RTL of FlipFlopD is
+begin
+	Q <= 
+	'0' when (reset='0') else
+	D when rising_edge(clock);
+	
+	
+--	process(clock)
+--	begin
+--		if (clock='1' and clock'event) then
+--			Q <= D;
+--		end if;
+--	end process;
+end RTL;
+
