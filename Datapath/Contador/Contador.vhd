ceb7b32b36f3971a0f1aa6e9fee14ae389743bc1
Luís Fernando Miki <69601772+luisfmiki@users.noreply.github.com>
2022-12-07 20:06:36 -0300

    Add files via upload

--- Datapath/Contador/Contador.vhd
+++ Datapath/Contador/Contador.vhd
@@ -5,7 +5,8 @@ use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity Contador is
     Port ( cnt,clk : in std_logic;
-           T: out std_logic;
+           T: out std_logic
+			 );
 end Contador;
 
 architecture count_arch of Contador is
@@ -19,20 +20,30 @@ component DivisorClock is
 
 end component;
   signal clk_1 : std_logic;
-  signal count : std_logic_vector(2 downto 0);
+  signal count : std_logic_vector(1 downto 0) := "00"	;
+
     begin
       div1 : DivisorClock port map (CLOCK_50MHz => clk, reset => '0', CLOCK_1Hz => clk_1);
 
-      process(cnt,clk_1,count)
-        begin
-          if ( cnt = '0') then 
-            count <= “000”;
-            T <= '0';
-          elsif(count = "111") then
-            T <= '1';
-          elsif (rising_edge(clk_1) and cnt = '1') then 
-            count <= count + 1;
-            T <= '0';
+      process(cnt,clk_1)
+        begin 
+            
+          if (rising_edge(clk_1) and cnt = '1') then 
+            count <= count + '1';
+			 elsif (cnt = '0' and rising_edge(clk_1)) then
+				count <= "00";
+--			 else
+--				count <= count;
           end if;
       end process;
-    end count_arch;
+		
+		process(count)
+			begin
+				if (count = "11") then
+					T <= '1';
+				else 
+					T <= '0';
+				end if;
+		end process;
+
+    end count_arch; 
